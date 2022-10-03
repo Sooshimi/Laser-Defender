48f28d3e91e53ff0275da6a50b0994eb2ad1ed9b
Sooshimi <masonchee93@gmail.com>
2022-10-03 13:22:04 +0100

    Added particle effects

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 // Holds the amount of health that the object this is attached to
 // Only takes damage if the other object is a DamageDealer
+// Instantiates particle effects when hit
 
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem hitEffect;
 
     // check if the other object is a damage dealer, and if it is, this object takes damage
     void OnTriggerEnter2D(Collider2D other) 
@@ -15,9 +17,11 @@ public class Health : MonoBehaviour
         // if other object has a DamageDealer component, then take damage
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
 
+        // take damage
         if (damageDealer != null)
         {
             TakeDamage(damageDealer.GetDamage());
+            PlayHitEffect();
             damageDealer.Hit();
         }
     }
@@ -30,5 +34,16 @@ public class Health : MonoBehaviour
             {
                 Destroy(gameObject);
             }
+    }
+
+    void PlayHitEffect()
+    {
+        if(hitEffect != null)
+        {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+
+            // destroy particle with a delay of the lifetime of the particle effect
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
+        }
     }
 }
