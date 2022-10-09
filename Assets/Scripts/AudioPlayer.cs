46df46dcdd2df5b2dec37dcfa1d23a2b2f038e3c
Sooshimi <masonchee93@gmail.com>
2022-10-09 20:28:22 +0100

    created singleton audio for bg music

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -12,6 +12,30 @@ public class AudioPlayer : MonoBehaviour
     [SerializeField] AudioClip damageClip;
     [SerializeField] [Range(0f, 1f)] float damageVolume = 1f; // constrained between 0 - 100%
 
+    // static variables persists through all instances of a class
+    // e.g. all AudioPlayer's in the scene will share this exact static instance
+    static AudioPlayer instance;
+    
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton() // allows audio player to persist to next scene
+    {
+        if (instance != null) // if this is NOT the first AudioPlayer to be instantiated, then destroy the audio
+        {
+            // disables it before destroying it on awake, in case other objects use the audio player
+            gameObject.SetActive(false); 
+            Destroy(gameObject);
+        }
+        else // if this is the first AudioPlayer to be instantiated, then persist it
+        {
+            instance = this; // then set the instance as this AudioPlayer (to persist it)
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
     public void PlayShootingClip()
     {
         PlayClip(shootingClip, shootingVolume);
